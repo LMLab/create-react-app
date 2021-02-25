aa98f34d6237c8d8f027b38f1cf2a528c5805bee
Mariyan Dimitrov <mariyan.di@finovation.com>
2021-02-25 09:43:41 +0200

    Renamed the serving app script

--- packages/react-scripts/scripts/serveApp.js
+++ packages/react-scripts/scripts/serveApp.js
@@ -4,19 +4,19 @@
 const openBrowser = require('react-dev-utils/openBrowser');
 const chalk = require('react-dev-utils/chalk');
 const paths = require('../config/paths');
-const app = require("https-localhost")()
+const app = require('https-localhost')();
 const express = require('express');
 
 const args = process.argv.slice(2);
-const portArgument = args.find(arg => arg.includes("PORT="));
-const SERVING_PORT = portArgument ? portArgument.split("=")[1] : 3333;
+const portArgument = args.find(arg => arg.includes('PORT='));
+const SERVING_PORT = portArgument ? portArgument.split('=')[1] : 3333;
 const SERVING_URL = `https://localhost:${SERVING_PORT}/`;
 
-function startServing () {
+function startServing() {
   app.use(express.static(paths.appBuild));
   app.listen(SERVING_PORT);
   console.log(chalk.green(`Production build is served on: ${SERVING_URL}`));
-  openBrowser(SERVING_URL)
+  openBrowser(SERVING_URL);
 }
 
 startServing();
